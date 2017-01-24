d0e260388573343ea514d4399834780d9bca2305
davidercoolio <davidjo286@nemi.tskoli.is>
2017-01-24 19:31:38 +0100

    Kláraði dæmi 3

--- Verkefni_Git.py
+++ Verkefni_Git.py
@@ -12,6 +12,28 @@ print(tala1 * tala2, "er margfeldið af tölunum")
 
 #Liður 2
 
+'''
 nafn1 = input("Sláðu inn fornafn: ")
 nafn2 = input("Sláðu inn eftirnafn: ")
 print("Halló", nafn1, nafn2)
+'''
+
+
+#Liður 3
+
+setning = input("Sláðu inn setningu: ")
+
+capital = 0
+lowcase = 0
+lowhigh = 0
+
+for x in range(len(setning)):
+    if setning[x].isupper():
+        capital += 1
+
+    elif setning[x].islower():
+        lowcase += 1
+
+        if x>0:
+            if setning[x-1].isupper():
+                lowhigh += 1
