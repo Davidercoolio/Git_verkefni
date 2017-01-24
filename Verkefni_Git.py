08794d428ecad0924c59470c3de7b1e49dc0c930
davidercoolio <davidjo286@nemi.tskoli.is>
2017-01-24 19:22:55 +0100

    Kláraði dæmi 1

---
+++ Verkefni_Git.py
@@ -0,0 +1,9 @@
+#Davíð Bjarki Jónsson 24-01-17
+
+# Liður 1
+
+tala1 = int(input("Sláðu inn fyrstu tölu: "))
+tala2 = int(input("Sláðu inn næstu tölu: "))
+print(tala1 + tala2, "er summan af tölunum")
+print(tala1 * tala2, "er margfeldið af tölunum")
+
