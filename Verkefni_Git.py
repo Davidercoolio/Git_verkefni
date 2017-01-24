2a02df0b25ee573190c6bc9ff63007af052b436a
davidercoolio <davidjo286@nemi.tskoli.is>
2017-01-24 19:25:58 +0100

    Kláraði dæmi 2

--- Verkefni_Git.py
+++ Verkefni_Git.py
@@ -1,9 +1,17 @@
 #Davíð Bjarki Jónsson 24-01-17
 
-# Liður 1
+#Liður 1
 
+'''
 tala1 = int(input("Sláðu inn fyrstu tölu: "))
 tala2 = int(input("Sláðu inn næstu tölu: "))
 print(tala1 + tala2, "er summan af tölunum")
 print(tala1 * tala2, "er margfeldið af tölunum")
+'''
 
+
+#Liður 2
+
+nafn1 = input("Sláðu inn fornafn: ")
+nafn2 = input("Sláðu inn eftirnafn: ")
+print("Halló", nafn1, nafn2)
